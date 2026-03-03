e2476bc416be3a582efcb2e4becf892ab9e34051
Akhil Bhadauria <akhilbhadauria1996@gmail.com>
2026-03-03 18:43:24 +0530

    Added initial commit

---
+++ src/main/java/com/akhil/simply_talk/SimplyTalkApplication.java
@@ -0,0 +1,14 @@
+package com.akhil.simply_talk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplyTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplyTalkApplication.class, args);
+		System.out.println("Simply Talk application started successfully!");
+	}
+
+}
