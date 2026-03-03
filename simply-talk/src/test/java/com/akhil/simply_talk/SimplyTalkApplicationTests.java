9bc458d5c066bb962ef82bdad1a6031f8873a271
Akhil Bhadauria <akhilbhadauria1996@gmail.com>
2026-03-03 18:31:18 +0530

    Added initial phase

---
+++ simply-talk/src/test/java/com/akhil/simply_talk/SimplyTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akhil.simply_talk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplyTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
